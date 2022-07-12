0412b75e039f738a040b2828ef459fb8f61bd620
Drownfe <palaciojuanfe@gmail.com>
2022-07-12 18:09:25 -0500

    Organize declarations

--- index.js
+++ index.js
@@ -1,24 +1,4 @@
-//Player constructor start
-function Player(name) {
-    this.name = name;
-    this.reward = reward;
-    this.cards = [];
-    this.cardSummatory = function() {
-        let sum = 0;
-        this.cards.forEach(element => {
-            sum += element.value;
-        })
-        return sum;
-    }
-    this.cardAdding = function(card) {
-        this.cards.push(card);
-    }
-    this.rewardAdding = function(reward) {
-        this.reward += reward;
-    }
-}
-//Card constructor start
-function Card() {
+function Card() { //Card constructor start
     let name;
     let suit;
     let value;
@@ -32,7 +12,7 @@ function Card() {
                     value = 10;
                     break;
                 default:
-                    let aceChoice = readlineSync.question('\n El repartidor te dio un As \n Ingrese 1 o 2 para elegir una opcion \n 1. A = 1 \n 2. A = 11 \n')
+                    let aceChoice = readlineSync.question('\n Acabas de recibir un As \n Ingrese 1 o 2 para elegir que valor deseas darle \n 1. A = 1 \n 2. A = 11 \n')
                     switch (Number(aceChoice)) {
                         case 1:
                             value = 1;
@@ -69,4 +49,24 @@ function Card() {
             return value;
         }
     });
+}
+
+//Player constructor start
+function Player(name) {
+    this.name = name;
+    this.reward = reward;
+    this.cards = [];
+    this.cardSummatory = function() {
+        let sum = 0;
+        this.cards.forEach(element => {
+            sum += element.value;
+        })
+        return sum;
+    }
+    this.cardAdding = function(card) {
+        this.cards.push(card);
+    }
+    this.rewardAdding = function(reward) {
+        this.reward += reward;
+    }
 }
